a88e126b7d8b11c728b0ae2a24376738360a00a5
Mcarreno <m-carreno@hotmail.com>
2022-06-09 22:05:06 -0400

    almost finisht the html layout

---
+++ foundation/Foundation-Sites-CSS-2/js/app.js
@@ -0,0 +1,10 @@
+// like botton function 
+$(document).foundation();
+
+$(function() {
+  $('.button-like')
+    .bind('click', function(event) {
+      $(".button-like").toggleClass("liked");
+    })
+});
+
